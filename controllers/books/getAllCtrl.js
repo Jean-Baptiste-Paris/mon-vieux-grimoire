10723b0a7798569462c2c1019d63e572049df499
Jean-Baptiste-Paris <ecrireajb@gmail.com>
2024-04-05 10:34:09 +0200

    Code cleaning and DESC sorting on allBooks request

--- controllers/books/getAllCtrl.js
+++ controllers/books/getAllCtrl.js
@@ -2,7 +2,7 @@ const Book = require('../../models/Book')
 
 const getAllBooks = async (req, res, next) => {
   try {
-    const books = await Book.find()
+    const books = await Book.find().sort({ _id: -1 })
     res.status(200).json(books)
   } catch (error) {
     res.status(400).json({ error })
--- middlewares/multer-config.js
+++ middlewares/multer-config.js
@@ -1,23 +1,4 @@
 const multer = require('multer')
 
-// const MIME_TYPES = {
-//   'image/jpg': 'jpg',
-//   'image/jpeg': 'jpg',
-//   'image/png': 'png',
-// }
-
 const storage = multer.memoryStorage()
 module.exports = multer({ storage: storage }).single('image')
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, callback) => {
-//     callback(null, 'uploads/images')
-//   },
-//   filename: (req, file, callback) => {
-//     const baseName = 'monvieuxgrimoire-picture-'
-//     const extension = MIME_TYPES[file.mimetype]
-//     callback(null, baseName + Date.now() + '.' + extension)
-//   },
-// })
-
-// module.exports = multer({ storage: storage }).single('image')
